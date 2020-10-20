6f8b2c7077c1b0c761a2c31fdc7a6daf7f24faca
Harshgupta182 <61505754+Harshgupta182@users.noreply.github.com>
2020-10-20 22:28:07 +0530

    Update operations.py

--- operations.py
+++ operations.py
@@ -247,3 +247,10 @@ def Tangent():
     x = int(x)
     res= math.tan(math.radians(x))
     return res
+n_num = [1, 2, 3, 4, 5] 
+n = len(n_num) 
+  
+get_sum = sum(n_num) 
+mean = get_sum / n 
+  
+print("Mean / Average is: " + str(mean)) 
